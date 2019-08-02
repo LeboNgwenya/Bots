86ba7a0e713c7140765d071f63b0ae8d1585dfc9
LeboNgwenya <50670015+LeboNgwenya@users.noreply.github.com>
2019-08-02 11:11:34 -0400

    Update scheduler.py

--- wellzestahealth-bots/src/executors/scheduler.py
+++ wellzestahealth-bots/src/executors/scheduler.py
@@ -1,6 +1,6 @@
 import schedule
 import time
-from src.services import PointsBotDispatcher, BirthdayBotDispatcher
+from src.services import PointsBotDispatcher, BirthdayBotDispatcher, PositivityBotDispcher, ReminderBotDispacher, UserEngagementDispatcher
 
 
 def send_birthday_message():
@@ -10,17 +10,24 @@ def send_birthday_message():
 def send_points_message():
     PointsBotDispatcher.call()
 
-def sent_positivity_message():
+def send_positivity_message():
     PositivityBotDispcher.call()
 
-#def send_reminder_message():
-# RemiderBotDispacher.cal()
+def send_reminder_message():
+    ReminderBotDispacher.call()
+
+def send_engagement_message():
+    UserEngagementDispatcher.call()
+
 
 if __name__ == '__main__':
     schedule.every().day.at("06:00").do(send_birthday_message)
     schedule.every(25).to(30)days.at("06:30").do(send_points_message)
-    Schedule.every().monday.at("06:00").do(sent_positivity_message)
+    schedule.every().monday.at("06:00").do(send_positivity_message)
+    schedule.every(25).to(30)days.at("08:00").do(send_engagement_message)
+
 
     while True:
         schedule.run_pending()
         time.sleep(1)
+
