096b7f6b2a2c91da652f1199cea574ff853d1e1e
LeboNgwenya <50670015+LeboNgwenya@users.noreply.github.com>
2019-08-28 17:35:03 -0400

    Update request.py

--- wellzestahealth-bots/src/utils/request.py
+++ wellzestahealth-bots/src/utils/request.py
@@ -2,5 +2,5 @@ import requests
 
 
 def request(query, variables):
-    request = requests.post('https://wellzesta-health-proxy-stg.herokuapp.com/graphql', json={'query': query, 'variables': variables}, headers={'api-key': 'token ny3oTYMz8kNEtk8o9dqGo8Xk'})
+    request = requests.post('*****Link*****', json={'query': query, 'variables': variables}, headers={'api-key': 'token'})
     return request.json()
